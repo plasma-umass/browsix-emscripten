6299fe69d6ca0aea9eb6f920eb54d8d276c97997
Bobby Powers <bobbypowers@gmail.com>
2016-03-29 18:07:58 -0400

    vendor syscall stuff

--- src/library_syscall.js
+++ src/library_syscall.js
@@ -163,6 +163,394 @@ var SyscallsLibrary = {
 #endif
       return ret;
     },
+#if EMTERPRETIFY_BROWSIX
+	  browsix: (function(){
+var exports = {};
+
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+
+
+(function (AF) {
+    AF[AF["UNSPEC"] = 0] = "UNSPEC";
+    AF[AF["LOCAL"] = 1] = "LOCAL";
+    AF[AF["UNIX"] = 1] = "UNIX";
+    AF[AF["FILE"] = 1] = "FILE";
+    AF[AF["INET"] = 2] = "INET";
+    AF[AF["INET6"] = 10] = "INET6";
+})(exports.AF || (exports.AF = {}));
+var AF = exports.AF;
+;
+(function (SOCK) {
+    SOCK[SOCK["STREAM"] = 1] = "STREAM";
+    SOCK[SOCK["DGRAM"] = 2] = "DGRAM";
+})(exports.SOCK || (exports.SOCK = {}));
+var SOCK = exports.SOCK;
+(function (ErrorCode) {
+    ErrorCode[ErrorCode["EPERM"] = 0] = "EPERM";
+    ErrorCode[ErrorCode["ENOENT"] = 1] = "ENOENT";
+    ErrorCode[ErrorCode["EIO"] = 2] = "EIO";
+    ErrorCode[ErrorCode["EBADF"] = 3] = "EBADF";
+    ErrorCode[ErrorCode["EACCES"] = 4] = "EACCES";
+    ErrorCode[ErrorCode["EBUSY"] = 5] = "EBUSY";
+    ErrorCode[ErrorCode["EEXIST"] = 6] = "EEXIST";
+    ErrorCode[ErrorCode["ENOTDIR"] = 7] = "ENOTDIR";
+    ErrorCode[ErrorCode["EISDIR"] = 8] = "EISDIR";
+    ErrorCode[ErrorCode["EINVAL"] = 9] = "EINVAL";
+    ErrorCode[ErrorCode["EFBIG"] = 10] = "EFBIG";
+    ErrorCode[ErrorCode["ENOSPC"] = 11] = "ENOSPC";
+    ErrorCode[ErrorCode["EROFS"] = 12] = "EROFS";
+    ErrorCode[ErrorCode["ENOTEMPTY"] = 13] = "ENOTEMPTY";
+    ErrorCode[ErrorCode["ENOTSUP"] = 14] = "ENOTSUP";
+})(exports.ErrorCode || (exports.ErrorCode = {}));
+var ErrorCode = exports.ErrorCode;
+var fsErrors = {
+    EPERM: 'Operation not permitted.',
+    ENOENT: 'No such file or directory.',
+    EIO: 'Input/output error.',
+    EBADF: 'Bad file descriptor.',
+    EACCES: 'Permission denied.',
+    EBUSY: 'Resource busy or locked.',
+    EEXIST: 'File exists.',
+    ENOTDIR: 'File is not a directory.',
+    EISDIR: 'File is a directory.',
+    EINVAL: 'Invalid argument.',
+    EFBIG: 'File is too big.',
+    ENOSPC: 'No space left on disk.',
+    EROFS: 'Cannot modify a read-only file system.',
+    ENOTEMPTY: 'Directory is not empty.',
+    ENOTSUP: 'Operation is not supported.',
+};
+var ApiError = (function () {
+    function ApiError(type, message) {
+        this.type = type;
+        this.code = ErrorCode[type];
+        if (message != null) {
+            this.message = message;
+        }
+        else {
+            this.message = fsErrors[type];
+        }
+    }
+    ApiError.prototype.toString = function () {
+        return this.code + ": " + fsErrors[this.code] + " " + this.message;
+    };
+    return ApiError;
+})();
+exports.ApiError = ApiError;
+function convertApiErrors(e) {
+    if (!e)
+        return e;
+    if (!e.hasOwnProperty('type') || !e.hasOwnProperty('message') || !e.hasOwnProperty('code'))
+        return e;
+    return new ApiError(e.type, e.message);
+}
+var SyscallResponse = (function () {
+    function SyscallResponse(id, name, args) {
+        this.id = id;
+        this.name = name;
+        this.args = args;
+    }
+    SyscallResponse.From = function (ev) {
+        if (!ev.data)
+            return;
+        for (var i = 0; i < SyscallResponse.requiredOnData.length; i++) {
+            if (!ev.data.hasOwnProperty(SyscallResponse.requiredOnData[i]))
+                return;
+        }
+        var args = ev.data.args.map(convertApiErrors);
+        return new SyscallResponse(ev.data.id, ev.data.name, args);
+    };
+    SyscallResponse.requiredOnData = ['id', 'name', 'args'];
+    return SyscallResponse;
+})();
+exports.SyscallResponse = SyscallResponse;
+var OnceEmitter = (function () {
+    function OnceEmitter() {
+        this.listeners = {};
+    }
+    OnceEmitter.prototype.once = function (event, cb) {
+        var cbs = this.listeners[event];
+        if (!cbs)
+            cbs = [cb];
+        else
+            cbs.push(cb);
+        this.listeners[event] = cbs;
+    };
+    OnceEmitter.prototype.emit = function (event) {
+        var args = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            args[_i - 1] = arguments[_i];
+        }
+        var cbs = this.listeners[event];
+        this.listeners[event] = [];
+        if (!cbs)
+            return;
+        for (var i = 0; i < cbs.length; i++) {
+            cbs[i].apply(null, args);
+        }
+    };
+    return OnceEmitter;
+})();
+var Process = (function (_super) {
+    __extends(Process, _super);
+    function Process(argv, environ) {
+        _super.call(this);
+        this.argv = argv;
+        this.env = environ;
+    }
+    Process.prototype.exit = function (code) {
+        if (code === void 0) { code = 0; }
+        syscall_1.syscall.exit(code);
+    };
+    return Process;
+})(OnceEmitter);
+
+var USyscalls = (function () {
+    function USyscalls() {
+        this.msgIdSeq = 1;
+        this.outstanding = {};
+        this.signalHandlers = {};
+            if (typeof self !== 'undefined') {
+		    self.onmessage = this.resultHandler.bind(this);
+
+	    }
+    }
+    USyscalls.prototype.exit = function (code) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i - 0] = arguments[_i];
+            }
+            console.log('received callback for exit(), should clean up');
+        };
+        this.post(msgId, 'exit', code);
+    };
+    USyscalls.prototype.kill = function (pid, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'kill', pid);
+    };
+    USyscalls.prototype.socket = function (domain, type, protocol, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'socket', domain, type, protocol);
+    };
+    USyscalls.prototype.getsockname = function (fd, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'getsockname', fd);
+    };
+    USyscalls.prototype.bind = function (fd, sockInfo, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'bind', fd, sockInfo);
+    };
+    USyscalls.prototype.listen = function (fd, backlog, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'listen', fd, backlog);
+    };
+    USyscalls.prototype.accept = function (fd, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'accept', fd);
+    };
+    USyscalls.prototype.connect = function (fd, addr, port, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'connect', fd, addr, port);
+    };
+    USyscalls.prototype.getcwd = function (cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'getcwd');
+    };
+    USyscalls.prototype.getpid = function (cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'getpid');
+    };
+    USyscalls.prototype.spawn = function (cwd, name, args, env, files, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'spawn', cwd, name, args, env, files);
+    };
+    USyscalls.prototype.pipe2 = function (flags, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'pipe2', flags);
+    };
+    USyscalls.prototype.getpriority = function (which, who, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'getpriority', which, who);
+    };
+    USyscalls.prototype.setpriority = function (which, who, prio, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'setpriority', which, who, prio);
+    };
+    USyscalls.prototype.open = function (path, flags, mode, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'open', path, flags, mode);
+    };
+    USyscalls.prototype.unlink = function (path, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'unlink', path);
+    };
+    USyscalls.prototype.utimes = function (path, atime, mtime, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'utimes', path, atime, mtime);
+    };
+    USyscalls.prototype.futimes = function (fd, atime, mtime, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'futimes', fd, atime, mtime);
+    };
+    USyscalls.prototype.rmdir = function (path, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'rmdir', path);
+    };
+    USyscalls.prototype.mkdir = function (path, mode, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'mkdir', path);
+    };
+    USyscalls.prototype.close = function (fd, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'close', fd);
+    };
+    USyscalls.prototype.pwrite = function (fd, buf, pos, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'pwrite', fd, buf, pos);
+    };
+    USyscalls.prototype.readdir = function (path, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'readdir', path);
+    };
+    USyscalls.prototype.fstat = function (fd, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'fstat', fd);
+    };
+    USyscalls.prototype.lstat = function (path, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'lstat', path);
+    };
+    USyscalls.prototype.stat = function (path, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'stat', path);
+    };
+    USyscalls.prototype.ioctl = function (fd, request, length, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'ioctl', fd, request, length);
+    };
+    USyscalls.prototype.readlink = function (path, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'readlink', path);
+    };
+    USyscalls.prototype.getdents = function (fd, length, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'getdents', fd, length);
+    };
+    USyscalls.prototype.pread = function (fd, length, offset, cb) {
+        var msgId = this.nextMsgId();
+        this.outstanding[msgId] = cb;
+        this.post(msgId, 'pread', fd, length, offset);
+    };
+    USyscalls.prototype.addEventListener = function (type, handler) {
+        if (!handler)
+            return;
+        if (this.signalHandlers[type])
+            this.signalHandlers[type].push(handler);
+        else
+            this.signalHandlers[type] = [handler];
+    };
+    USyscalls.prototype.resultHandler = function (ev) {
+        var response = SyscallResponse.From(ev);
+        if (!response) {
+            console.log('bad usyscall message, dropping');
+            console.log(ev);
+            return;
+        }
+        if (response.name) {
+            var handlers = this.signalHandlers[response.name];
+            if (handlers) {
+                for (var i = 0; i < handlers.length; i++)
+                    handlers[i](response);
+            }
+            else {
+                console.log('unhandled signal ' + response.name);
+            }
+            return;
+        }
+        this.complete(response.id, response.args);
+    };
+    USyscalls.prototype.complete = function (id, args) {
+        var cb = this.outstanding[id];
+        delete this.outstanding[id];
+        if (cb) {
+            cb.apply(undefined, args);
+        }
+        else {
+            console.log('unknown callback for msg ' + id + ' - ' + args);
+        }
+    };
+    USyscalls.prototype.nextMsgId = function () {
+        return ++this.msgIdSeq;
+    };
+    USyscalls.prototype.post = function (msgId, name) {
+        var args = [];
+        for (var _i = 2; _i < arguments.length; _i++) {
+            args[_i - 2] = arguments[_i];
+        }
+        self.postMessage({
+            id: msgId,
+            name: name,
+            args: args,
+        });
+    };
+    return USyscalls;
+})();
+		  exports.syscall = new USyscalls();
+
+		  exports.process = new Process(null, null);
+
+		  var cb = (function(proc) {
+			  return function(data) {
+				  var args = data.args[0];
+				  var environ = data.args[1];
+				  args = [args[0]].concat(args);
+				  proc.argv = args;
+				  proc.env = environ;
+				  setTimeout(function () { proc.emit('ready'); }, 0);
+			  }
+		  })(exports.process);
+
+		  exports.syscall.addEventListener('init', cb);
+
+		  if (typeof self !== 'undefined')
+			  self.process = process;
+		  return exports;
+	  }()),
+#endif // EMTERPRETIFY_BROWSIX
 #if NO_FILESYSTEM == 0
     getStreamFromFD: function() {
       var stream = FS.getStream(SYSCALLS.get());
@@ -232,7 +620,7 @@ var SyscallsLibrary = {
           return err ? -1 : len;
         });
       };
-      $syscall.internal.pwrite.apply($syscall.internal, [fd, h.slice(off, off+count), 0, done]);
+      $syscall.internal.pwrite(fd, h.slice(off, off+count), 0, done);
     });
   },
   __syscall5: function(which, varargs) { // open
@@ -767,7 +1155,6 @@ var SyscallsLibrary = {
         return 0;
 
       var lenRead = 0;
-      var sys = $syscall.internal;
 
       function readOne() {
         var buf = bufs.shift();
@@ -787,7 +1174,7 @@ var SyscallsLibrary = {
             });
           }
         };
-        sys.pread(fd, buf.length, 0, done);
+        $syscall.internal.pread(fd, buf.length, 0, done);
       }
       readOne();
     });
@@ -820,7 +1207,6 @@ var SyscallsLibrary = {
         return 0;
 
       var written = 0;
-      var sys = $syscall.internal;
 
       function writeOne() {
         var buf = bufs.shift();
@@ -838,7 +1224,7 @@ var SyscallsLibrary = {
             });
           }
         };
-        sys.pwrite.apply(sys, [fd, buf, 0, done]);
+        $syscall.internal.pwrite(fd, buf, 0, done);
       }
       writeOne();
     });
