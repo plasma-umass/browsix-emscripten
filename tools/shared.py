b59f680bb652d1d33f7bddf90e967df56cba3471
Bobby Powers <bobbypowers@gmail.com>
2016-05-02 15:23:31 -0400

    tools/shared: allow implicit function declarations for lmbench

--- tools/shared.py
+++ tools/shared.py
@@ -900,7 +900,7 @@ if LLVM_TARGET == WASM_TARGET:
 #   -Wno-error=implicit-function-declaration
 # or disable even a warning about it with
 #   -Wno-implicit-function-declaration
-COMPILER_OPTS += ['-Werror=implicit-function-declaration']
+#COMPILER_OPTS += ['-Werror=implicit-function-declaration']
 
 USE_EMSDK = not os.environ.get('EMMAKEN_NO_SDK')
 
