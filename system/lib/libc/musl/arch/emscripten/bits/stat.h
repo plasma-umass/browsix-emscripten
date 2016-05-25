5ff7af000199d388a021bc21549aaf4375778dd5
Bobby Powers <bobbypowers@gmail.com>
2016-05-25 09:49:23 -0400

    libc: change struct padding to match 64-bit version Browsix kernel knows about

--- system/lib/libc/musl/arch/emscripten/bits/stat.h
+++ system/lib/libc/musl/arch/emscripten/bits/stat.h
@@ -4,19 +4,27 @@
 struct stat
 {
 	dev_t st_dev;
-	int __st_dev_padding;
-	long __st_ino_truncated;
-	mode_t st_mode;
+	dev_t __st_dev_pad;
+	ino_t st_ino;
+	ino_t __st_ino_pad;
 	nlink_t st_nlink;
+	nlink_t __st_nlink_pad;
+
+	mode_t st_mode;
 	uid_t st_uid;
 	gid_t st_gid;
+	unsigned int    __pad0;
 	dev_t st_rdev;
-	int __st_rdev_padding;
+	dev_t __st_rdev_pad;
 	off_t st_size;
+	off_t __st_size_pad;
 	blksize_t st_blksize;
+	blksize_t __st_blksize_pad;
 	blkcnt_t st_blocks;
+	blkcnt_t __st_blocks_pad;
+
 	struct timespec st_atim;
 	struct timespec st_mtim;
 	struct timespec st_ctim;
-	ino_t st_ino;
+	long long __unused[3];
 };
