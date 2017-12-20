ce840ef584b41268585bdb364b5d3905255d6c93
Bobby Powers <bobbypowers@gmail.com>
2017-12-20 16:38:39 -0500

    browsix: SYSCALLS.browsix -> BROWSIX.browsix
    Will fix stuttering in future revision.

--- src/postamble.js
+++ src/postamble.js
@@ -493,7 +493,7 @@ if (!ENVIRONMENT_IS_PTHREAD) // EXIT_RUNTIME=0 only applies to default behavior 
 
 #if BROWSIX
 if (ENVIRONMENT_IS_BROWSIX) {
-  self.onmessage = SYSCALLS.browsix.syscall.resultHandler.bind(SYSCALLS.browsix.syscall);
+  self.onmessage = BROWSIX.browsix.syscall.resultHandler.bind(BROWSIX.browsix.syscall);
   Runtime.process.once('ready', function() {
     Module['thisProgram'] = Runtime.process.argv[0];
     for (var k in Runtime.process.env) {
