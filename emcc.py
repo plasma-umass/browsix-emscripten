ec90895e901b717d6d4b47675d9e50391e622dc2
Bobby Powers <bobbypowers@gmail.com>
2018-01-05 11:41:40 -0800

    fix .js target identification

--- emcc.py
+++ emcc.py
@@ -1840,14 +1840,14 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
           to_replace = """var wasmTextFile = Module['wasmTextFile'] || '%s.wast';
   var wasmBinaryFile = Module['wasmBinaryFile'] || '%s.wasm';
   var asmjsCodeFile = Module['asmjsCodeFile'] || '%s.temp.asm.js';""" % (prefix, prefix, prefix)
-          js_contents = slurp(target + '.js')
+          js_contents = slurp(prefix + '.js')
           if js_contents.find(to_replace) < 0:
             logging.error("Couldn't fixup JS+WASM file %s/%s", target, wasm_binary_target)
             os.exit(1)
           js_contents = js_contents.replace(to_replace, to_replace + '\n' + wasm_content)
           with open(target + '.js', 'w') as f:
             f.write(js_contents)
-          print 'embedded wasm in JS: %s %d' % (target, len(wasm_content))
+          # print 'embedded wasm in JS: %s %d' % (target, len(wasm_content))
 
         if final_suffix in EXECUTABLE_SUFFIXES and target != js_target:
           with open(target, 'w') as f:
