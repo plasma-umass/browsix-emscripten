bb247b339a6ad5d7fabb24835b258e4e8d0d8cef
Bobby Powers <bobby@stripe.com>
2018-12-25 19:42:37 -0500

    fixup

--- system/lib/libc/musl/arch/emscripten/bits/stat.h
+++ system/lib/libc/musl/arch/emscripten/bits/stat.h
@@ -4,27 +4,19 @@
 struct stat
 {
 	dev_t st_dev;
-	dev_t __st_dev_pad;
-	ino_t st_ino;
-	ino_t __st_ino_pad;
-	nlink_t st_nlink;
-	nlink_t __st_nlink_pad;
-
+	int __st_dev_padding;
+	long __st_ino_truncated;
 	mode_t st_mode;
+	nlink_t st_nlink;
 	uid_t st_uid;
 	gid_t st_gid;
-	unsigned int    __pad0;
 	dev_t st_rdev;
-	dev_t __st_rdev_pad;
+	int __st_rdev_padding;
 	off_t st_size;
-	off_t __st_size_pad;
 	blksize_t st_blksize;
-	blksize_t __st_blksize_pad;
 	blkcnt_t st_blocks;
-	blkcnt_t __st_blocks_pad;
-
 	struct timespec st_atim;
 	struct timespec st_mtim;
 	struct timespec st_ctim;
-	long long __unused[3];
+	ino_t st_ino;
 };
